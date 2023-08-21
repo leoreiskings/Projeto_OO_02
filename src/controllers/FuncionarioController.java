53349145cd88ef75481f1500b2066aeb621f2787
leoreiskings <leo19reis@gmail.com>
2023-08-21 11:29:38 -0300

    alteração final - geração de arquivo txt sem bugs

--- src/controllers/FuncionarioController.java
+++ src/controllers/FuncionarioController.java
@@ -23,10 +23,10 @@ public class FuncionarioController {
 		Funcionario funcionario = new Funcionario();
 
 		funcionario.setSetor(new Setor()); // inicializando o vinculo de setor do funcionario.
-																		 // Criando uma nova instancia do objeto setor dentro do obj funcionario
+											// Criando uma nova instancia do objeto setor dentro do obj funcionario
 
-		funcionario.setFuncoes(new ArrayList<Funcao>()); // inicializando o vinculo de funcoes do funcionario.		
-		
+		funcionario.setFuncoes(new ArrayList<Funcao>()); // inicializando o vinculo de funcoes do funcionario.
+
 		System.out.print("Informe o id do funcionário.........: ");
 		funcionario.setId(Integer.parseInt(scanner.nextLine()));
 
@@ -42,40 +42,41 @@ public class FuncionarioController {
 		System.out.print("Informe o salário do funcionário....: ");
 		funcionario.setSalario(Double.parseDouble(scanner.nextLine()));
 
-		System.out.println("Informe o Id do Setor do Funcionario: ");
+		System.out.print("Informe o id do setor..............: ");
 		funcionario.getSetor().setId(Integer.parseInt(scanner.nextLine()));
 
-		System.out.println("Informe o Nome do Setor do Funcionario: ");
-		funcionario.getSetor().setNome(scanner.nextLine()); // navegando pelo obj setor 'getSetor()' e jogando valor
-															// para o nome do setor 'setNome()'
-															// primeiro nos alocamos memoria para o obj Setor dentro do
-															// Funcionario que tem esse relacionamento
-															// modelado na classe Funcionario. Lá vemos a relação
-															// 1-FUNCIONARIO TEM 1-SETOR
+		System.out.print("Informe o nome do setor...........: ");
+		funcionario.getSetor().setNome(scanner.nextLine());
+		// navegando pelo obj setor 'getSetor()' e jogando valor
+		// para o nome do setor 'setNome()'
+		// primeiro nos alocamos memoria para o obj Setor dentro do
+		// Funcionario que tem esse relacionamento
+		// modelado na classe Funcionario. Lá vemos a relação
+		// 1-FUNCIONARIO TEM 1-SETOR
 
 		System.out.print("Informe o número de funções.......: ");
 		Integer qtdFuncoes = Integer.parseInt(scanner.nextLine());
-
+		
 		for (int i = 0; i < qtdFuncoes; i++) {
 			
-			//cria um novo obj funcao a cada passada do "for"
-			Funcao funcao = new Funcao(); 
+			// variável de instância / objeto
+			Funcao funcao = new Funcao();
 			
-			System.out.println("Informe o ID da Funcao: ");
+			System.out.print("Informe o id da função..............: ");
 			funcao.setId(Integer.parseInt(scanner.nextLine()));
 			
-			System.out.println("Informe o Descricao da Funcao: ");
+			System.out.print("Informe a descrição da função.......: ");
 			funcao.setDescricao(scanner.nextLine());
 			
-			funcionario.getFuncoes().add(funcao);			
-			
+			//adicionar a função para o funcionário
+			funcionario.getFuncoes().add(funcao);
 		}
-		
+
 		FuncionarioRepository funcionarioRepository = new FuncionarioRepository();
-		
+
 		funcionarioRepository.exportarFuncionario(funcionario);
-		
-		System.out.println("\nDados Exportados com Sucesso!");	
+
+		System.out.println("\nDados Exportados com Sucesso!");
 
 	}
 
